0020db51c10fa0fe71b0e46a068126c38953a1e4
William Jenkins <wfjenkins@ucsd.edu>
2025-05-13 00:38:09 -0700

    Adjust log format

--- src/rirpy/__init__.py
+++ src/rirpy/__init__.py
@@ -6,7 +6,7 @@ import sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s.%(msecs)03d|%(levelname)s|%(message)s",
+    format="%(asctime)s.%(msecs)03d|%(levelname)s| %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
